8aeba9676fea6cc82ef656ad70e3658af4ef1f72
rfaguiar <rfaguiar1@gmail.com>
2018-04-18 11:02:42 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 
+for item, value in os.environ.items():
+    print('{}: {}'.format(item, value))
+	
+MYSQL_HOST = os.environ.get('MYSQL_HOST')
+MYSQL_USER = os.environ.get('MYSQL_USER')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+MYSQL_DB = os.environ.get('MYSQL_DB')
+MYSQL_PORT = os.environ.get('MYSQL_PORT')
+
 SECRET_KEY = "secret_jogoteca"
-MYSQL_HOST = "0.0.0.0"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "root"
-MYSQL_DB = "jogoteca"
-MYSQL_PORT = 3306
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
