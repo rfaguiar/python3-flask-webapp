67e1d73c8a9c1a9cb4fb36fb5bf0fae451e74cf5
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-04-18 22:03:36 -0300

    config bd heroku

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-for item, value in os.environ.items():
-    print('{}: {}'.format(item, value))
+# for item, value in os.environ.items():
+#     print('{}: {}'.format(item, value))
 	
 MYSQL_HOST = os.environ.get('MYSQL_HOST')
 MYSQL_USER = os.environ.get('MYSQL_USER')
 MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
 MYSQL_DB = os.environ.get('MYSQL_DB')
-MYSQL_PORT = os.environ.get('MYSQL_PORT')
+MYSQL_PORT = int(os.environ.get('MYSQL_PORT'))
 
 SECRET_KEY = "secret_jogoteca"
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
