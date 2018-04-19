c7829b5c7c95fb5c3768503f8b00d3b8b5c6a77d
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-04-18 22:10:26 -0300

    config bd heroku

--- jogoteca.py
+++ jogoteca.py
@@ -1,19 +1,19 @@
-from flask import Flask
-from flask_mysqldb import MySQL
-import os
-
-os.environ['MYSQL_HOST'] = "0.0.0.0"
-os.environ['MYSQL_USER'] = "root"
-os.environ['MYSQL_PASSWORD'] = "root"
-os.environ['MYSQL_DB'] = "jogoteca"
-os.environ['MYSQL_PORT'] = "3306"
-
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-db = MySQL(app)
-
-
-from views import *
-
-if __name__ == '__main__':
-    app.run(debug=True, host="localhost", port=8080)
+# from flask import Flask
+# from flask_mysqldb import MySQL
+# import os
+#
+# os.environ['MYSQL_HOST'] = "0.0.0.0"
+# os.environ['MYSQL_USER'] = "root"
+# os.environ['MYSQL_PASSWORD'] = "root"
+# os.environ['MYSQL_DB'] = "jogoteca"
+# os.environ['MYSQL_PORT'] = "3306"
+#
+# app = Flask(__name__)
+# app.config.from_pyfile('config.py')
+# db = MySQL(app)
+#
+#
+# from views import *
+#
+# if __name__ == '__main__':
+#     app.run(debug=True, host="localhost", port=8080)
